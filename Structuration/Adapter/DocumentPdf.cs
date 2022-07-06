0b5c4dab23953922034ef86d8cfca426ccdfa383
MarionPlayout <marion.playout@gmail.com>
2022-07-06 22:48:03 +0200

    examples and images for singleton and adapter

---
+++ Structuration/Adapter/DocumentPdf.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class DocumentPdf : Document
+{
+  protected ComposantPdf outilPdf = new ComposantPdf();
+
+  public string contenu
+  {
+     set
+     {
+       outilPdf.pdfFixeContenu(value);
+     }
+  }
+
+  public void dessine()
+  {
+    outilPdf.pdfPrepareAffichage();
+    outilPdf.pdfRafraichit();
+    outilPdf.pdfTermineAffichage();
+  }
+
+  public void imprime()
+  {
+    outilPdf.pdfEnvoieImprimante();
+  }
+}
